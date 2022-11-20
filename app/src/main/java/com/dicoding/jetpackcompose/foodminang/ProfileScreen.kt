acdc4f9d93d8c06e7a7f2ab85e0aacb85fcb29a8
Rahmathidayat4299 <hrahmat950@gmail.com>
2022-11-20 22:44:55 +0700

    add image

--- app/src/main/java/com/dicoding/jetpackcompose/foodminang/ProfileScreen.kt
+++ app/src/main/java/com/dicoding/jetpackcompose/foodminang/ProfileScreen.kt
@@ -2,12 +2,14 @@ package com.dicoding.jetpackcompose.foodminang
 
 
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import coil.compose.AsyncImage
 
 /**
  * Created by Rahmat Hidayat on 20/11/2022.
@@ -20,6 +22,12 @@ fun ProfileScreen(
         modifier = modifier.fillMaxSize(),
         contentAlignment = Alignment.Center,
     ) {
-        Text(stringResource(R.string.menu_profile))
+        Column() {
+            AsyncImage(model = "https://raw.githubusercontent.com/Rahmathidayat4299/asset_compose_dicoding/main/rahmathidayat.png", contentDescription ="profil" )
+            Text(stringResource(R.string.menu_profile))
+            Text(stringResource(R.string.nama))
+            Text(stringResource(R.string.email))
+
+        }
     }
 }
